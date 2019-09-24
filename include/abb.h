464a2b9b01c97ded5f31e2cd5289dbb194493108
rannaraabe <rannaraabe@hotmail.com>
2019-09-24 11:28:49 -0300

    search ok

--- include/abb.h
+++ include/abb.h
@@ -27,28 +27,28 @@ public:
     /**
      * Função de busca por um valor em árvore binária
      */ 
-    void search(pont_no pt, int value, int f){
+    int search(pont_no pt, int value, int f){
         if(pt != nullptr){
-            if(pt.chave = value)
-                f = 1;      // Chave encontrada e pt aponta para nó onde a chave se encontra
+            if(pt->chave = value)
+                return f = 1;      // Chave encontrada e pt aponta para nó onde a chave se encontra
             else
             {
-                if(value < pt.chave){
-                    if(pt.esq = nullptr)
-                        f = 2;      // Chave não encontrada e pt aponta para nó cuja árvore esquerda é vazia
+                if(value < pt->chave){
+                    if(pt->esq = nullptr)
+                        return f = 2;      // Chave não encontrada e pt aponta para nó cuja árvore esquerda é vazia
                     else 
-                        pt = pt.esq;
+                        pt = pt->esq;
                 } else {
-                    if(pt.dir = nullptr)
-                        f = 3;      // // Chave não encontrada e pt aponta para nó cuja árvore direita é vazia
+                    if(pt->dir = nullptr)
+                        return f = 3;      // // Chave não encontrada e pt aponta para nó cuja árvore direita é vazia
                     else
-                        pt = pt.dir;
+                        pt = pt->dir;
                 }
                 if(f < 1)
                     search(pt, value, f);
             } 
         } else 
-            f = 0;      // Árvore vazia
+            return f = 0;      // Árvore vazia
     }
 
     /**
