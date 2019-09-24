df1b5206b1c7716d63fcc29ab045bbea72563a39
rannaraabe <rannaraabe@hotmail.com>
2019-09-24 14:07:07 -0300

    metodo insert

--- include/abb.h
+++ include/abb.h
@@ -4,6 +4,7 @@
  */ 
 
 #include <iostream>
+using namespace std;
 
 /**
  * Struct que representa o nó
@@ -24,23 +25,25 @@ public:
     typedef struct node no;     // Definindo um nó do tipo node
     typedef no *pont_no;        // Definindo um ponteiro para o nó
 
+    no *raiz;    // Nó raiz
+
     /**
      * Função de busca por um valor em árvore binária
      */ 
-    int search(pont_no pt, int value, int f){
+    void search(pont_no pt, int value, int f){
         if(pt != nullptr){
             if(pt->chave = value)
-                return f = 1;      // Chave encontrada e pt aponta para nó onde a chave se encontra
+                f = 1;      // Chave encontrada e pt aponta para nó onde a chave se encontra
             else
             {
                 if(value < pt->chave){
                     if(pt->esq = nullptr)
-                        return f = 2;      // Chave não encontrada e pt aponta para nó cuja árvore esquerda é vazia
+                        f = 2;      // Chave não encontrada e pt aponta para nó cuja árvore esquerda é vazia
                     else 
                         pt = pt->esq;
                 } else {
                     if(pt->dir = nullptr)
-                        return f = 3;      // // Chave não encontrada e pt aponta para nó cuja árvore direita é vazia
+                        f = 3;      // Chave não encontrada e pt aponta para nó cuja árvore direita é vazia
                     else
                         pt = pt->dir;
                 }
@@ -48,20 +51,40 @@ public:
                     search(pt, value, f);
             } 
         } else 
-            return f = 0;      // Árvore vazia
+            f = 0;      // Árvore vazia
     }
 
     /**
      * Função de inserção de um nó em uma árvore binária
      */ 
-    void insert(){
+    void insert(pont_no pt, int value, int f){
+        pont_no pt_aux = nullptr; 
+        
+        search(pt, value, f);   // Busca o valor na árvore, antes de adicionar
+        if(f == 1)
+            cout << "Valor já está na árvore";
+        else {
+            // ocupar(pt1);
+            pt_aux->chave = value;
+            pt_aux->esq = nullptr;
+            pt_aux->dir = nullptr;
 
+            if(f == 0)
+                this->raiz = nullptr; // Árvore vazia
+            else {
+                if(f == 2)
+                    pt->esq = pt_aux;
+                else 
+                    pt->dir = pt_aux;
+            }
+        }
+            
     }
 
     /**
      * Função de remoção de um nó em uma árvore binária
      */ 
-    void remove(){
+    void remove(pont_no pt, int value, int f){
 
     }
 };
