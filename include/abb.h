14642d86a0a3b825b864109cc96480c7de9bad66
rannaraabe <rannaraabe@hotmail.com>
2019-09-24 17:16:52 -0300

    arquivos e remove

--- include/abb.h
+++ include/abb.h
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <string>
+#include <queue>
 using namespace std;
 
 /**
@@ -13,7 +14,6 @@ using namespace std;
 struct node
 {
     int chave;
-    int conteudo;
     struct node *esq;
     struct node *dir;      
 };
@@ -21,13 +21,11 @@ struct node
 class abb
 {
 private:
-    /* data */
-public:
     typedef struct node no;     // Definindo um nó do tipo node
     typedef no *pont_no;        // Definindo um ponteiro para o nó
-
     no *raiz;    // Nó raiz
 
+public:
     /**
      * Função de busca por um valor em árvore binária
      */ 
@@ -59,24 +57,24 @@ public:
      * Função de inserção de um nó em uma árvore binária
      */ 
     void insert(pont_no pt, int value, int f){
-        pont_no pt_aux = nullptr; 
-        
+        pont_no pt_aux = nullptr; // Ponteiro auxiliar
         search(pt, value, f);   // Busca o valor na árvore, antes de adicionar
+
         if(f == 1)
             cout << "Valor já está na árvore";
         else {
-            // ocupar(pt1);
+            // ocupar(pt_aux);
             pt_aux->chave = value;
             pt_aux->esq = nullptr;
             pt_aux->dir = nullptr;
 
             if(f == 0)
-                this->raiz = nullptr; // Árvore vazia
+                this->raiz = nullptr;   // Árvore vazia
             else {
                 if(f == 2)
-                    pt->esq = pt_aux;
+                    pt->esq = pt_aux;   // Chave na esquerda
                 else 
-                    pt->dir = pt_aux;
+                    pt->dir = pt_aux;   // Chave na direita
             }
         }
             
@@ -85,7 +83,44 @@ public:
     /**
      * Função de remoção de um nó em uma árvore binária
      */ 
-    void remove(pont_no pt, int value, int f){
+    node* remove(pont_no pt, int value, int f){
+        pont_no pt_aux = nullptr; // Ponteiro auxiliar
+
+        if(pt == nullptr)
+            return nullptr;   
+
+        if(pt->esq == nullptr && pt->dir == nullptr){
+            if(pt->chave == value)
+                return nullptr;
+            else
+                return pt;
+        } 
+
+        queue<node*> fila;
+        fila.push(pt);
+
+        // else {
+        //     pt_aux->chave = pt->chave;
+        //     pt_aux->esq = pt->esq;
+        //     pt_aux->dir = pt->dir;
+
+        //     if(pt_aux->chave == value){
+        //         if (pt_aux->esq == nullptr)
+        //             pt_aux->dir;
+        //         else if(pt_aux->dir == nullptr)
+        //             pt_aux->esq;
+        //         else {
+                    
+        //         }
+        //     }
+        // }
+
+    }
+
+    /**
+     * Função imprime a árvore
+     */ 
+    void print(){
 
     }
 
